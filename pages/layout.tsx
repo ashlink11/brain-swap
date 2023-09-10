7f1ff506fde77bfcfd691cbfbd164975190fa822
Ashley Grevelink <ashleyg@web3.foundation>
2023-09-10 16:28:55 -0400

    tested locally with yarn build

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,9 +1,4 @@
-import type { Metadata } from 'next'
- 
-// These styles apply to every route in the application
-import '../styles/globals.css'
 
- 
 export default function RootLayout({
   children,
 }: {
